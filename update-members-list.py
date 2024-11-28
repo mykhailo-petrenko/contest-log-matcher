eaee728f3d874ecdd214e519d13d38d47b220891
Mykhailo Petrenko <mikael.petrenko@gmail.com>
2024-11-28 01:01:41 +0100

    load_member_list should return PI4NTC list with typles (NR, call, call2, start_date)

--- update-members-list.py
+++ update-members-list.py
@@ -2,8 +2,6 @@ import urllib.request
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
-# https://pi4ntc.nl/wp-admin/admin-ajax.php?action=memberlist
-
 
 def clear_row(row):
     return row.replace('\\n', '').replace('\\t', '').strip()
@@ -11,18 +9,30 @@ def clear_row(row):
 
 def load_member_list(url):
     http = urllib.request.urlopen(url)
-    raw = str(http.read())
-    members_raw = raw.split('|')
-    print("----")
+    raw = http.read().decode('utf-8')
+    members_raw = raw.strip().split('|')
+
+    members = []
+
     for member_raw in members_raw:
         member_raw = clear_row(member_raw)
-        properties = member_raw.split('\n')
+
+        if not member_raw:
+            continue
+
+        properties = member_raw.split(',')
         properties = [property.strip() for property in properties]
-        print(properties)
+
+        members.append(tuple(properties[1:]))
+
+    return members
+
+
 
 def main():
     url = "https://pi4ntc.nl/wp-admin/admin-ajax.php?action=memberlist"
-    load_member_list(url)
+    members = load_member_list(url)
+    print("\n".join([",".join(m) for m in members]))
 
 
 if __name__ == "__main__":
